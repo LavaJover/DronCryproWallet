739959c2093097c656f3ef0de1e0d2a5a62c06e8
bodya <lavajover@gmail.com>
2025-01-20 07:22:45 +0300

    refactor

---
+++ api-gateway/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main(){
+	authServiceConn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil{
+		log.Fatalf("Failed to connect to auth-service: %v", err)
+	}
+
+	defer authServiceConn.Close()
+}
