5099a21075d520c254228b17705da2e44d330744
bodya <lavajover@gmail.com>
2025-01-23 12:38:19 +0300

    feat: add http handler to get user wallets by jwt token

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"bytes"
 
 	"github.com/LavaJover/DronCryptoWallet/api-gateway/internal/config"
 	"github.com/LavaJover/DronCryptoWallet/api-gateway/models"
@@ -201,6 +203,124 @@ func main(){
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(response)
 	})
+
+
+	// Ручка получения списка кошельков пользователя
+	http.HandleFunc("/api/wallet/process-user-wallets", func(w http.ResponseWriter, r *http.Request) {
+		myLog.Info("/api/wallet/process-user-wallets", "method", r.Method)
+
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		if r.Method != http.MethodPost {
+			myLog.Error("method is not supported", "method", r.Method)
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Чтение тела запроса
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		type RequestPayload struct{
+			Token string `json:"token"`
+		}
+
+		// Парсим входящий запрос
+		var requestPayload RequestPayload
+		if err := json.Unmarshal(body, &requestPayload); err != nil {
+			http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+			return
+		}
+
+		response, err := walletServiceClient.GetUserPrivateKeys(context.Background(), &walletpb.GetUserPrivateKeysRequest{Token: requestPayload.Token})
+
+		if err != nil{
+			myLog.Error("failed to get user private keys")
+			http.Error(w, "Failed to get user private keys", http.StatusBadRequest)
+			return
+		}
+
+		privateKeys := response.PrivateKeys
+
+		// Структура запроса
+		type WalletRequest struct {
+			PrivateKeys []string `json:"privateKeys"`
+		}
+		
+		// Структура для получения ответа от /generate-wallets
+		type WalletResponse struct {
+			Addresses []struct {
+				PrivateKey string `json:"privateKey"`
+				Address    string `json:"address"`
+				Error      string `json:"error,omitempty"`
+			} `json:"addresses"`
+		}
+
+		walletRequest := WalletRequest{
+			PrivateKeys: privateKeys,
+		}
+		requestBody, err := json.Marshal(walletRequest)
+		if err != nil {
+			http.Error(w, "Failed to marshal request", http.StatusInternalServerError)
+			return
+		}
+
+		resp, err := http.Post("http://localhost:3000/generate-addresses", "application/json", bytes.NewBuffer(requestBody))
+		if err != nil {
+			http.Error(w, "Failed to send request to wallet service", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		// Чтение ответа от /generate-wallets
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "Failed to read response from wallet service", http.StatusInternalServerError)
+			return
+		}
+
+		// Парсим ответ от wallet service
+		var walletResponse WalletResponse
+		if err := json.Unmarshal(respBody, &walletResponse); err != nil {
+			myLog.Error("failed to get response from node wallet service: " + err.Error())
+			http.Error(w, "Invalid response from wallet service", http.StatusInternalServerError)
+			return
+		}
+
+		// Сохраняем список адресов
+		var walletAddresses []string
+		for _, wallet := range walletResponse.Addresses {
+			if wallet.Error == "" {
+				walletAddresses = append(walletAddresses, wallet.Address)
+			} else {
+				log.Printf("Error generating address for private key %s: %s", wallet.PrivateKey, wallet.Error)
+			}
+		}
+
+		// Возвращаем список адресов в ответ
+		http_response := map[string]interface{}{
+			"walletAddresses": walletAddresses,
+		}
+
+
+		myLog.Info(r.URL.Path, "status", http.StatusOK)
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(http_response)
+	})
+
 
 	// Запуск сервера
 	myLog.Info("api gateway serving", "address", cfg.Address)
